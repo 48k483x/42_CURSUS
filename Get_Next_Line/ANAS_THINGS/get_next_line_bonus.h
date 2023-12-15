dbfe1f955d6628250e40e68c66be27515be4ef65
achahrou <achahrou@e2r2p3.1337.ma>
2023-12-15 17:31:11 +0100

    FINISH MENDATORY AND BONUS PART IN GNL

---
+++ Get_Next_Line/ANAS_THINGS/get_next_line_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achahrou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 07:13:19 by achahrou          #+#    #+#             */
+/*   Updated: 2023/12/06 11:22:36 by achahrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10 
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}				t_list;
+
+int		found_newline(t_list *list);
+t_list	*find_lst_node(t_list *list);
+char	*get_line(t_list *list);
+void	copy_str(t_list *list, char *str);
+int		len_to_newline(t_list *list);
+void	polish_list(t_list **list);
+char	*get_next_line(int fd);
+void	dealloc(t_list **list, t_list *clean_node, char *buf);
+void	create_list(t_list **list, int fd);
+void	append(t_list **list, char *buf, int fd);
+
+#endif
