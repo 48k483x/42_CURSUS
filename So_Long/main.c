26d245573a441681e23b6fb02e951efa11739953
achahrou <achahrou@c4r1p1.1337.ma>
2023-12-28 19:01:16 +0100

    showing an tab for the first time congrats to me

--- So_Long/main.c
+++ So_Long/main.c
@@ -1,10 +1,27 @@
-#include "mlx.h"
+#include <mlx.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MLX_ERROR 1
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 300
+
 int main() {
     void *mlx;
+    void *win;
+
     mlx = mlx_init();
+    if (NULL == mlx)
+	    return (MLX_ERROR);
+    win = mlx_new_window(mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "My first thing ever");
+    if (NULL == win)
+    {
+    	free(win);
+	return (MLX_ERROR);
+    }
+    while (1)
+	    ;
+    mlx_destroy_window(mlx ,win);
     mlx_destroy_display(mlx);
     free(mlx);
 }
