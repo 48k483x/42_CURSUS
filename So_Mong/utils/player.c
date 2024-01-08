bc1a66b7ea069e7b7de73e4e540993c0ec5782dd
48k483x <romaissaeabde@gmail.com>
2024-01-07 22:43:02 -0800

    just an update for norminette

--- So_Mong/utils/player.c
+++ So_Mong/utils/player.c
@@ -2,95 +2,95 @@
 
 int exit_game(t_data *data, char *s)
 {
-    perror(s);
-    mlx_destroy_window(data->mlx, data->win);
-    exit(1);
-    return (1);
+	perror(s);
+	mlx_destroy_window(data->mlx, data->win);
+	exit(1);
+	return (1);
 }
 
 int init_player(t_data *data)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    while (i < data->map_hei)
-    {
-        j = 0;
-        while (j < data->map_wid)
-        {
-            if (data->map[i][j] == 'P')
-            {
-                data->player_x = j;
-                data->player_y = i;
-                data->map[i][j] = '0';
-                return (1);
-            }
-            j++;
-        }
-        i++;
-    }
-    data->start_x = data->player_x;
-    data->start_y = data->player_y;
-    return (0);
+	i = 0;
+	while (i < data->map_hei)
+	{
+		j = 0;
+		while (j < data->map_wid)
+		{
+			if (data->map[i][j] == 'P')
+			{
+				data->player_x = j;
+				data->player_y = i;
+				data->map[i][j] = '0';
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	data->start_x = data->player_x;
+	data->start_y = data->player_y;
+	return (0);
 }
 
-
-void move_player(int key,t_data *data)
+void move_player(int key, t_data *data)
 {
-    int old_x;
-    int old_y;
+	int old_x;
+	int old_y;
 
-    old_x = data->player_x;
-    old_y = data->player_y;
-    all_moves(key, data);    
-    if (data->player_x < 0 || data->player_x >= data->map_wid ||
-        data->player_y < 0 || data->player_y >= data->map_hei ||
-        data->map[data->player_y][data->player_x] == '1') 
-    {
-        data->player_x = old_x;
-        data->player_y = old_y;
-    }
-    else if (data->map[data->player_y][data->player_x] == 'X')
-        exit_game(data, "You Lose");
-    else if (data->map[data->player_y][data->player_x] == 'C')
-    {
-        data->collectible--;
-        data->map[data->player_y][data->player_x] = '0';
-        draw_background(data);
-    }
-    else if (data->map[data->player_y][data->player_x] == 'E' && data->collectible == 0)
-        exit_game(data, "You Won");
-    draw_background(data);
-    draw_game(data);
+	old_x = data->player_x;
+	old_y = data->player_y;
+	all_moves(key, data);
+	if (data->player_x < 0 || data->player_x >= data->map_wid ||
+		 data->player_y < 0 || data->player_y >= data->map_hei ||
+		 data->map[data->player_y][data->player_x] == '1')
+	{
+		data->player_x = old_x;
+		data->player_y = old_y;
+	}
+	else if (data->map[data->player_y][data->player_x] == 'X')
+		exit_game(data, "You Lose");
+	else if (data->map[data->player_y][data->player_x] == 'C')
+	{
+		data->collectible--;
+		data->map[data->player_y][data->player_x] = '0';
+		draw_background(data);
+	}
+	else if (data->map[data->player_y][data->player_x] == 'E' &&
+				data->collectible == 0)
+		exit_game(data, "You Won");
+	draw_background(data);
+	draw_game(data);
 }
 
 int key_press(int key, t_data *data)
 {
-    if (data->reached == 1)
-    {
-        if (key == W && data->map[data->player_y - 1][data->player_x] != '1')
-        {
-            move_player(W, data);
-            data->moves++;
-        }
-        else if (key == S  && data->map[data->player_y + 1][data->player_x] != '1')
-        {
-            move_player(S, data);
-            data->moves++;
-        }
-        else if (key == A && data->map[data->player_y][data->player_x - 1] != '1')
-        {
-            move_player(A, data);
-            data->moves++;
-        }
-        else if (key == D && data->map[data->player_y][data->player_x + 1] != '1')
-        {
-            move_player(D, data);
-            data->moves++;
-        }
-    }
-   if (key == ESC)
-        exit_game(data, "You Exited Game");
-    return (0);
+	if (data->reached == 1)
+	{
+		if (key == W && data->map[data->player_y - 1][data->player_x] != '1')
+		{
+			move_player(W, data);
+			data->moves++;
+		}
+		else if (key == S && data->map[data->player_y + 1][data->player_x] != '1')
+		{
+			move_player(S, data);
+			data->moves++;
+		}
+		else if (key == A && data->map[data->player_y][data->player_x - 1] != '1')
+		{
+			move_player(A, data);
+			data->moves++;
+		}
+		else if (key == D && data->map[data->player_y][data->player_x + 1] != '1')
+		{
+			move_player(D, data);
+			data->moves++;
+		}
+	}
+	if (key == ESC)
+		exit_game(data, "You Exited Game");
+	return (0);
 }
--- So_Mong/utils/twichiat.c
+++ So_Mong/utils/twichiat.c
@@ -2,59 +2,60 @@
 
 void collectible_function(char **filenames)
 {
-    filenames[0] = "assets/coll/1.xpm";
-    filenames[1] = "assets/coll/2.xpm";
-    filenames[2] = "assets/coll/3.xpm";
-    filenames[3] = "assets/coll/4.xpm";
-    filenames[4] = "assets/coll/5.xpm";
-    filenames[5] = "assets/coll/6.xpm";
+	filenames[0] = "assets/coll/1.xpm";
+	filenames[1] = "assets/coll/2.xpm";
+	filenames[2] = "assets/coll/3.xpm";
+	filenames[3] = "assets/coll/4.xpm";
+	filenames[4] = "assets/coll/5.xpm";
+	filenames[5] = "assets/coll/6.xpm";
 }
 
 int standar_animation(t_data *data)
 {
-    char *n;
+	char *n;
 
-    n = ft_itoa(data->moves);
-    data->frame_counter++;
-    draw_game(data);
-    mlx_string_put(data->mlx, data->win, 50, 50, 0xFFFF00, "Moves Number : ");
-    mlx_string_put(data->mlx, data->win, 210, 50, 0xFFFF00, ft_itoa(data->moves));
-    free(n);
-    return (0);
+	n = ft_itoa(data->moves);
+	data->frame_counter++;
+	draw_game(data);
+	mlx_string_put(data->mlx, data->win, 50, 50, 0xFFFF00, "Moves Number : ");
+	mlx_string_put(data->mlx, data->win, 210, 50, 0xFFFF00,
+						ft_itoa(data->moves));
+	free(n);
+	return (0);
 }
 
 int ft_strlen(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-        i++;
-    return (i);
+	while (str[i])
+		i++;
+	return (i);
 }
 
 int check_map_row_len(t_data *data)
 {
-    int i = 0;
-    int len = ft_strlen(data->map[0]);
+	int i = 0;
+	int len = ft_strlen(data->map[0]);
 
-    while (data->map[i])
-    {
-        if (ft_strlen(data->map[i]) != len)
-            return (0);
-        i++;
-    }
-    return (1);
+	while (data->map[i])
+	{
+		if (ft_strlen(data->map[i]) != len)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 void free_visited(t_data *data, int **visited)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (i < data->map_hei)
-    {
-        free(visited[i]);
-        i++;
-    }
-    free(visited);
+	i = 0;
+	while (i < data->map_hei)
+	{
+		free(visited[i]);
+		i++;
+	}
+	free(visited);
 }
