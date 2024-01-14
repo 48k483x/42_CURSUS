94b61716240769e4ce4060edc1fcdd23cea1bd90
FROST - 48K483X <abderrahmanech@outlook.fr>
2024-01-14 08:02:54 -0500

    Finish Mendatory Part Of Pipex Project

--- PiPex/pipex.c
+++ PiPex/pipex.c
@@ -1,30 +1,46 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "pipex.h"
 
-int main()
+void    openf_check(t_data *pipex, char *f1, char *f2)
 {
-    int fd[2];
-    pid_t pid;
+    if ((pipex->fd2 = open(f2, O_WRONLY | O_CREAT | O_TRUNC)) < 0)
+        exiti("Error In Open File For Writing\n");
+    if ((pipex->fd1 = open(f1, O_RDONLY)) < 0)
+        exiti("Error In Open File For Reading\n");
+    if (access(f1, R_OK) < 0)
+        exiti("Error Reading File\n");
+    if (access(f2, W_OK) < 0)
+        exiti("Error Writing File\n");
+}
 
-    pipe(fd);
-    pid = fork();
-
-    if (pid == 0)
+int main(int ac, char **av)
+{
+    t_data pipex;
+    if (ac != 5)
+        exiti("Incorrect Args Num\n");
+    openf_check(&pipex, av[1], av[4]);
+    pipex.cmd1 = ft_split(av[2], ' ');   // must be freed
+    pipex.cmd2 = ft_split(av[3], ' ');  // must be freed
+    pipex.path = str_concat("/bin/", pipex.cmd1[0]); // must be freed
+    pipex.spath = str_concat("/bin/",pipex.cmd2[0]);// must be freed
+    if (pipe(pipex.fd) < 0)
+        exiti("Pipe Error\n");
+    if ((pipex.pid = fork()) < 0)
+        exiti("Fork Error\n");
+    else if (pipex.pid == 0)
     {
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[0]);
-        char *cmd1[] = { "ls", NULL };
-        execvp(cmd1[0], cmd1);
+        dup2(pipex.fd1, STDIN_FILENO);
+        close(pipex.fd1);
+        dup2(pipex.fd[1], STDOUT_FILENO);  // 1
+        close(pipex.fd[0]);
+        execve(pipex.path, pipex.cmd1, NULL);
     }
     else
     {
         wait(NULL);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[1]);
-        char *cmd2[] = { "wc", "-l", NULL };
-        execvp(cmd2[0], cmd2);
+        dup2(pipex.fd2, STDOUT_FILENO);
+        close(pipex.fd2);
+        dup2(pipex.fd[0], STDIN_FILENO);  // 0
+        close(pipex.fd[1]);
+        execve(pipex.spath, pipex.cmd2, NULL);
     }
-
-    return 0;
 }
---
+++ PiPex/utils.c
@@ -0,0 +1,67 @@
+#include "pipex.h"
+
+void    exiti(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+int ft_strlen(char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+char    *str_concat(char *s1, char *s2)
+{
+    char    *concat;
+    int     i;
+    int     j;
+
+    i = 0;
+    j = 0;
+    concat = malloc(sizeof(char *) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+    if (!concat)
+        return (NULL);
+    while (s1[i])
+    {
+        concat[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+    {
+        concat[i] = s2[j];
+        j++;
+        i++;
+    }
+    return (concat);
+}
+
+/*
+    We splited to the utils part
+    exiti(You Custom Error Messg);
+        function that we handle errors in
+        out pipex program, it send an mssg
+        to stdout then it exit;
+    as we know the is 8 ways for an 
+    process to terminate 5 Normal are :
+        exit();
+        return ; from the main
+        _exit || _Exit
+        return of the last thread from its 
+                        start Routine
+        calling pthread_exit from the last 
+                            thread.
+    then we used the ft_strlen() to use it
+    in the str_concat function.
+    cause wow among all exec functions
+    they choosed the execve that work with the 
+    pathname not filename. So we need the path
+    that is stored in /bin/filename of the command
+    so to do it i did the str_concat to concat 
+    the command name and it path.
+ */
