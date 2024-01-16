79eeeb55f16b6b2b87dab741b2c900f7cf3fb149
FROST - 48K483X <abderrahmanech@outlook.fr>
2024-01-16 07:26:04 -0500

    finish

---
+++ PiPex/Bonus/pipex.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abkabex <abkabex@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 10:39:31 by achahrou          #+#    #+#             */
+/*   Updated: 2024/01/15 23:33:24 by abkabex          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void openf_check(t_data *pipex, char *f1, char *f2)
+{
+	if ((pipex->fd2 = open(f2, O_WRONLY | O_CREAT | O_TRUNC)) < 0)
+		exiti("Error In Open File For Writing\n");
+	if ((pipex->fd1 = open(f1, O_RDONLY)) < 0)
+		exiti("Error In Open File For Reading\n");
+	if (access(f1, R_OK) < 0)
+		exiti("Error Reading File\n");
+	if (access(f2, W_OK) < 0)
+		exiti("Error Writing File\n");
+}
+
+void    child_p(t_data *pipex, int ac)
+{
+    dup2(pipex->fd1, 0);
+	if (pipex->i != ac - 2)
+		dup2(pipex->fd[1], 1);
+	else
+	{
+		dup2(pipex->fd2, 1);
+		close(pipex->fd2);
+	}
+	close(pipex->fd[0]);
+	execve(pipex->path, pipex->cmd, NULL);
+	exit(EXIT_FAILURE);
+}
+
+void    parent_p(t_data *pipex)
+{
+    wait(NULL);
+	close(pipex->fd[1]);
+	pipex->fd1 = pipex->fd[0];
+}
+
+
+int main(int ac, char **av)
+{
+	t_data	pipex;
+
+	pipex.i = 2;
+	openf_check(&pipex, av[1], av[ac -1]);
+	while (pipex.i < ac - 1)
+	{
+		pipex.cmd = ft_split(av[pipex.i], ' ');
+		pipex.path= str_concat("/bin/", pipex.cmd[0]);
+		if (pipe(pipex.fd) < 0)
+			exiti("Fork Error\n");
+		if ((pipex.pid = fork())< 0)
+			exiti("Fork Error\n");
+		else if (pipex.pid == 0) 
+			child_p(&pipex, ac);
+		else
+			parent_p(&pipex);
+		pipex.i++;
+	}
+}
---
+++ PiPex/Bonus/pipex.h
@@ -0,0 +1,35 @@
+#ifndef PIPEX_H
+#define PIPEX_H
+
+# include <unistd.h> // dup2, pipe, close, execvp
+# include <sys/types.h> // pid_t
+# include <sys/wait.h> // wait
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+typedef struct s_pipex
+{
+    char **cmd;
+    int fd[2];
+    pid_t pid;
+    int fd1;
+    int fd2;
+    char *path;
+    char *spath;
+    int cmd_nums;
+    int i;
+}   t_data;
+
+/* split Functions */
+int     count_strings(char const *s, char c);
+char    *malloc_strings(const char *s, char c);
+char    **ft_split(char const *s, char c);
+
+/* utils Function and if you need any explaination you can read it file itself */
+void    exiti(char *s);
+char    *str_concat(char *s1, char *s2);
+int     ft_strlen(char *s);
+
+#endif // PIPEX_H
