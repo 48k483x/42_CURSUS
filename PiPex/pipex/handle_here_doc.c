3b6684bd7fd2098d644aa8ba3a103959ae9352ea
FROST - 48K483X <abderrahmanech@outlook.fr>
2024-01-19 07:00:14 -0500

    finish with  handled and comment added and everything is ready in shaa lah , lets pray , its friday, then lets push and train in the gym, today is leg day , never skip leg day man

--- PiPex/pipex/handle_here_doc.c
+++ PiPex/pipex/handle_here_doc.c
@@ -1,5 +1,12 @@
 #include "pipex.h"
-
+/* 
+This function checks if the program is working with
+a here document or not. If the first argument is
+"here_doc", it calls the handle_heredoc function 
+and increments the command index. to let the i point 
+to the first cmd this the exemple : we first have i = 2
+./pipex here_doc "limiter"  "cmd"  outfile
+     0      1         2 ++    3       4*/
 void	check_heredoc(t_data *pipex, char **av, int ac)
 {
 	if (ft_strcmp(av[1], "here_doc") == 0)
@@ -13,10 +20,15 @@ void	check_heredoc(t_data *pipex, char **av, int ac)
 		pipex->infile = av[1];
 	}
 }
+/* 
+This function handles the "here document" input 
+redirection. It reads lines from standard input 
+until it encounters the limiter, writing each 
+line to a temporary file named "here_doc".
+*/
 
 void	handle_heredoc(t_data *pipex, char *limiter)
 {
-	printf("pipex->infile: %s\n", pipex->infile);
 	pipex->line = NULL;
 	pipex->fd_hd = open("here_doc", O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (pipex->fd_hd < 0)
--- PiPex/pipex/pipex.c
+++ PiPex/pipex/pipex.c
@@ -6,12 +6,17 @@
 /*   By: abkabex <abkabex@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 10:39:31 by achahrou          #+#    #+#             */
-/*   Updated: 2024/01/19 04:10:47 by abkabex          ###   ########.fr       */
+/*   Updated: 2024/01/19 06:39:40 by abkabex          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
+/* 
+This function opens the input and output files.
+checks if the files can be opened for RD and WR,
+and if not, it exiti() lia mn lprogram with an 
+error message.
+*/
 void	openf_check(t_data *pipex, char *f1, char *f2)
 {
 	pipex->fd2 = open(f2, O_WRONLY | O_CREAT | O_TRUNC, \
@@ -27,6 +32,16 @@ void	openf_check(t_data *pipex, char *f1, char *f2)
 		exiti("Error Writing File\n");
 }
 
+/* this function is for the child process, so its when 
+   the pid == 0, it check first if its an here_doc, if 
+   its, it will redirect the stdin (0) to the here_doc
+   file, if its not, it will do the same thing with the
+   fd1 of the file f1, then it check if its not the last
+   cmd, then it duplicate the stdout to the RD fd of the 
+   pipe, if it's the last, then we redirect stdout to the
+   output file, then we close the RD fd of the pipe, then
+   we call execve to execute the cmd, if there is an error 
+   with the command it will just exit */
 void	child_p(t_data *pipex, int ac, char *output_file)
 {
 	if (ft_strcmp(pipex->infile, "here_doc") != 0)
@@ -53,13 +68,24 @@ void	child_p(t_data *pipex, int ac, char *output_file)
 	execve(pipex->path, pipex->cmd, NULL);
 	exit(EXIT_FAILURE);
 }
-
+/* i close the write end of the pipe, then i redirect
+the input to the read end of the pipe for the next command
+so the input of the next cmd will be fd1, that is 
+the read end of the pipe , cause we alrady did in our code 
+if its not the last redirect the stdout to fd[1] that is the 
+write end of the pip */
 void	parent_p(t_data *pipex)
 {
 	close(pipex->fd[1]);
 	pipex->fd1 = pipex->fd[0];
 }
-
+/* its just an function to check path for the command,
+   we get the env variable using getenv();
+   getenv("PATH") is used to get the value of the PATH 
+   environment variable. The PATH environment variable 
+   is a list of directories where the system looks for 
+   executables when a command is run. then we strat checking
+   if its it the right path of the command then store it*/
 void	check_path(t_data *pipex, char *p1)
 {
 	if (p1 == NULL)
@@ -88,7 +114,7 @@ void	check_path(t_data *pipex, char *p1)
 			exiti("Error In Path\n");
 	}
 }
-
+/* The main function, where everything start hahaha */
 int	main(int ac, char **av)
 {
 	t_data	pipex;
