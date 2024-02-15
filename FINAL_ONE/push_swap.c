2393302b345a2c58146813647a8d3501ad7f969c
frost <romaissaeabde@gmail.com>
2024-02-15 03:06:57 +0100

    still working in this inshallah we finish before 5am

--- FINAL_ONE/push_swap.c
+++ FINAL_ONE/push_swap.c
@@ -3,56 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abkabex <abkabex@student.42.fr>            +#+  +:+       +#+        */
+/*   By: achahrou <achahrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 03:24:38 by achahrou          #+#    #+#             */
-/*   Updated: 2024/02/14 20:36:30 by abkabex          ###   ########.fr       */
+/*   Updated: 2024/02/15 03:04:29 by achahrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	exit_with_message(char *s)
-{
-	while (*s)
-		write(2, s++, 1);
-	exit(1);
-}
-
-void	free_all(t_stack *a, char **arr)
-{
-	free_split(arr);
-	free_stack(&a);
-}
-
-bool	is_space(char c)
-{
-	return (c == ' ' || (c >= 9 && c <= 13));
-}
-
-int	arr_n(char **arr, char **av)
-{
-	int	i;
-	int	j;
-	
-	i = 1;
-	while (av[i])
-	{
-		j = 0;
-		while (av[i][j] && is_space(av[i][j]))
-			j++;
-		if (av[i][j] == '\0')
-		{
-			free_split(arr);
-			exit_with_message("Error\n");
-		}
-		i++;
-	}
-	i = 0;
-	while (arr[i])
-		i++;
-	return (i);
-}
 
 int	main(int ac, char **av)
 {
---
+++ FINAL_ONE/utils/main_utils.c
@@ -0,0 +1,43 @@
+#include"../push_swap.h"
+
+void	exit_with_message(char *s)
+{
+	while (*s)
+		write(2, s++, 1);
+	exit(1);
+}
+
+void	free_all(t_stack *a, char **arr)
+{
+	free_split(arr);
+	free_stack(&a);
+}
+
+bool	is_space(char c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int	arr_n(char **arr, char **av)
+{
+	int	i;
+	int	j;
+	
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j] && is_space(av[i][j]))
+			j++;
+		if (av[i][j] == '\0')
+		{
+			free_split(arr);
+			exit_with_message("Error\n");
+		}
+		i++;
+	}
+	i = 0;
+	while (arr[i])
+		i++;
+	return (i);
+}
